156e38bc17f770c74c3c07755c7778da08a22165
taebin91 <31372480+taebin91@users.noreply.github.com>
2025-11-30 21:14:39 +0900

    Update tvWikiAdblock.js

--- tvWikiAdblock.js
+++ tvWikiAdblock.js
@@ -441,7 +441,7 @@
     // ---------------------------------------------------
 
     // 기존의 기타 포커스 비활성화 로직 (안전을 위해 유지)
-    document.querySelectorAll('a.img, img, img.lazy, iframe', 'a.on').forEach(element => {
+    document.querySelectorAll('a.img, img, img.lazy, iframe', 'a.on', '.filter_layer').forEach(element => {
         element.setAttribute('tabindex', '-1');
     });
 
