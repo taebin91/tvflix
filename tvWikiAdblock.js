b971d82dd9a3d4bdd7e217e04d01bc4cd64d19ef
taebin91 <31372480+taebin91@users.noreply.github.com>
2025-12-01 07:51:07 +0900

    Update tvWikiAdblock.js

--- tvWikiAdblock.js
+++ tvWikiAdblock.js
@@ -393,31 +393,12 @@
             border-radius: 0px !important;
             outline: 4px solid #1E90FF !important;/* Dodger Blue */
             outline-offset: 0px !important;
-            box-shadow: 
-                0 0 0 4px #1E90FF inset, 
+            box-shadow:
+                0 0 0 4px #1E90FF inset,
                 0 0 8px rgba(30,144,255,1) !important;
             transition: outline-color 0.2s, box-shadow 0.2s !important;
-
-
-            
         }
-        
-        /* 모든 포커스 가능한 요소의 테두리 스타일을 재정의 */
-        :focus {
 
-            z-index: 9999 !important;
-            background-color: #FFD700 !important; /* 노란색 배경 */
-            outline: 4px solid #FFD700 !important;
-            outline-offset: 0px !important;
-
-            box-shadow:
-                0 0 0 4px #FFD700 inset,
-                0 0 8px rgba(255, 215, 0, 1) !important;
-
-            transition: outline-color 0.2s, box-shadow 0.2s;
-        }
-        
-        
 
         /* iFrame 포커스 스타일 제거 및 시각적으로 숨기기 */
         iframe:focus {
